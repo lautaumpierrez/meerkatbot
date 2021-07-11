4eb70c86684f5dec7c8f3780dc21d975f2dbd7c4
Lautaro Umpierrez <lauta.guillermo@gmail.com>
2021-07-10 22:22:57 -0300

    Updating description & version

---
+++ README.md
@@ -0,0 +1,2 @@
+# MEERCAT LIBRARY
+> It is an extra layout to discord.py library, writen to improve the discord bots architecture.
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import find_packages, setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='meerkatbot',
     packages=find_packages(include=["meerkatbot"]),
-    version='0.1.0',
+    version='0.1.0.2',
     description='MeerkatBot is an extra layout to discord py',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Lautaro Umpiérrez',
     license='MIT',
     install_requires=['discord', 'asyncio'],
